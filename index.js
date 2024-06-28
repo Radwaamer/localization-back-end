30695299bb4eb8aa8799488a018c06a9e38fc511
radwa amer <rororado44@gmail.com>
2024-06-28 12:23:23 -0700

    add files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const translate = require('translate-google');
+
+const app = express();
+app.use(express.json());
+
+app.post('/translate', async(req,res)=>{
+    const {msg,to}= req.body;
+    try {
+        const translation = await translate(msg, { from: 'auto', to: to });
+        res.status(200).json({ translation });
+    } catch (error) {
+        res.status(500).json({ error: 'Translation failed' });
+    }
+});
+
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
