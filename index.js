3faf19eb6fceb75ac5a33c13e74a57254718716f
radwa amer <rororado44@gmail.com>
2024-06-29 19:17:19 -0700

    add except

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.post('/translate', async(req,res)=>{
     const {msg,to}= req.body;
     try {
-        const translation = await translate(msg, { from: 'auto', to: to });
+        const translation = await translate(msg, { from: 'auto', to: to, except:[] });
         res.status(200).json({ translation });
     } catch (error) {
         res.status(500).json({ error: 'Translation failed' });
